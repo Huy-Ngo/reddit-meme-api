610bd1928445917726d92325fb68e11c6ca55ccc
Huy-Ngo <duchuy29092000@gmail.com>
2020-08-30 17:42:50 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from json import load
+from random import choice
+
+from praw import Reddit
+from flask import Flask
+
+with open('config.json', 'r') as f:
+    data = load(f)
+    client_id = data['client_id']
+    client_secret = data['client_secret']
+    user_agent = data['user_agent']
+
+app = Flask(__name__)
+
+reddit = Reddit(client_id=client_id, client_secret=client_secret,
+                user_agent=user_agent)
+
+
+@app.route('/<sub>')
+def memes(sub):
+    meme_list = {}
+    for submission in reddit.subreddit(sub).hot(limit=10):
+        if submission.selftext == '' and 'redd.it' in submission.url:
+            meme_list[submission.id] = {
+                "permalink": submission.permalink,
+                "url": submission.url,
+                "author": submission.author.name,
+                "score": submission.score
+            }
+    return meme_list
+
